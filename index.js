0d7389802dbc93e694fc9cae29d9bf1c9080ccbd
JohnMWood <jmw.home@gmail.com>
2018-12-22 13:59:05 -0800

    Added passport authentication callback

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID, 
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'auth/google/callback'
-    },  accessToken => {
-            console.log(accessToken);
+        callbackURL: '/auth/google/callback'
+    },  (accessToken, refreshToken, profile, done) => {
+            console.log('accessToken', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
         }
     )
 );
+
+app.get(
+    '/auth/google', // route 
+    passport.authenticate('google', { 
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'));
