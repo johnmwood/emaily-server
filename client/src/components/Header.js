92d8b2f4085e0d5b550e8a95ff580b5061a7db36
JohnMWood <jmw.home@gmail.com>
2019-03-18 20:09:59 -0600

    added webhook integration and localtunneling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -24,7 +24,7 @@ class Header extends Component {
     render() {
         return (
             <nav>
-                <div className="nav-wrapper teal darken-4">
+                <div className="nav-wrapper">
                     <Link 
                         to={this.props.auth ? '/surveys' : '/'} 
                         className="left brand-logo"
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -37,4 +37,9 @@ module.exports = app => {
     }
     
   });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body); 
+    res.send({}); 
+  }); 
 };
